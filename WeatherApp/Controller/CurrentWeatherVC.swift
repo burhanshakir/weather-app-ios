322edd12c4eee1aaad66ea6bde8e1915585356c5
bshakir <burhanshakir96@gmail.com>
2018-01-15 18:26:44 +0000

    Creating UI

---
+++ WeatherApp/Controller/CurrentWeatherVC.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentWeatherVC.swift
+//  WeatherApp
+//
+//  Created by Burhanuddin Shakir on 15/01/18.
+//  Copyright © 2018 Burhanuddin Shakir. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+}
